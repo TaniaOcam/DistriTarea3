2ac12a5dd23f86c49baf1874043ed6eab184d712
TaniaOcam <87431933+TaniaOcam@users.noreply.github.com>
2022-10-02 20:59:33 -0400

    Add files via upload

---
+++ scripts/client.sh
@@ -0,0 +1,2 @@
+source ..
+mvn clean install compile exec:java -Dexec.mainClass="py.una.pol.distritarea3.Client.UDPClient"
---
+++ scripts/server.bat
@@ -0,0 +1,3 @@
+cd ..
+mvn clean install compile exec:java -Dexec.mainClass="py.una.pol.distritarea3.Server.UDPServer"
+cd /scripts
